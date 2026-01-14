fb0adc61f89c11a3fa79acb710cea814a99814b4
SoDeS <49758901+s0d3s@users.noreply.github.com>
2026-01-14 19:20:45 +0200

    ci: disable free-threading builds on all platforms

--- cygwin_cibuildwheel_build.sh
+++ cygwin_cibuildwheel_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-FORCE_CYGWIN_ENV=true PYTHONUTF8=1 CIBW_SKIP="cp36*" CIBW_BUILD="cp3*" cibuildwheel --platform windows
+FORCE_CYGWIN_ENV=true PYTHONUTF8=1 CIBW_SKIP="cp3??t-*" CIBW_BUILD="cp3*" cibuildwheel --platform windows
